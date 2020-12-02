ae87af3f325ef0c847d57ad5a8d37bbfd7cb1a30
Roel Stuurman <Roel.Stuurman@deltares.nl>
2020-12-02 13:03:25 +0100

    Day 1 part 1

---
+++ Day01/day1-part1-output.ps1
@@ -0,0 +1,31 @@
+
+$list = Get-Content -Path .\day1-Input.txt 
+
+$sumammount = 2020
+$halfsummaccount = $sumammount / 2
+
+$listHigh = @()
+$listLow = @()
+
+Foreach ($number in $list) {
+    if ([int]$number -gt $halfsummaccount) {
+        write-output $number
+        $listHigh += [int]$number
+    }
+    else {
+        write-outout "low"
+        $listlow += [int]$number    
+    }
+}
+
+Foreach ($numberhigh in $listHigh) {
+    foreach ($numberlow in $listLow) {
+        if (($numberlow + $numberhigh) -eq 2020) {
+            $result = $numberhigh * $numberlow
+            write-output "Uitkomst van $numberhigh keer $numberlow  is $result"
+            break 
+        }
+    }
+}
+
+$number | gm
